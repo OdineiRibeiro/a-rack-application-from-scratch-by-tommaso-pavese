bcd1a14898fdfd9b986714d733b4289b9f57c4c9
Odinei Rocha <odinei.ribeiro@live.com>
2017-04-17 12:31:45 -0300

    added code to base controller

---
+++ app/controllers/base_controller.rb
@@ -0,0 +1,35 @@
+class BaseController
+  attr_reader :request
+
+  def initialize(request)
+    @request = request
+  end
+
+  def index
+    body = '
+      <html>
+        <head><title>A Rack Demonho</title></head>
+        <body>
+          <h1>This is the root page</h1>
+          <p>Hello from a controller!</p>
+        </body>
+      </html>
+    '
+
+    build_response body
+  end
+
+  private
+
+  def build_response(body, status: 200)
+    [status, { 'Content-Type' => 'text/html' }, [body]]
+  end
+
+  def redirect_to(uri)
+    [302, { 'Location' => uri }, []]
+  end
+
+  def params
+    request.params
+  end
+end
